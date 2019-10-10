5b62cccd49bc5865997aaba5a89370540bf43891
Nishad Sohoni <natorious1990@gmail.com>
2019-10-10 21:16:45 +0100

    Added alembic init script

---
+++ sql/alembiccing/alembic/versions/458bef86413e_noop.py
@@ -0,0 +1,24 @@
+"""noop
+
+Revision ID: 458bef86413e
+Revises:
+Create Date: 2019-10-10 21:13:25.128765
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '458bef86413e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
