20f87499a57d6695933ac3606a3acea4e503915c
Stefano Borini <stefano.borini@gmail.com>
2019-10-20 22:48:54 +0100

    Added mixin

--- design-patterns/mixin.py
+++ design-patterns/mixin.py
@@ -1,1 +1,81 @@
-# Add add-on mixin and participant mixin
+# add-on mixin
+
+class Post:
+    def __init__(self, author, title, text):
+        super().__init__()
+        self.author = author
+        self.title = title
+        self.text = text
+
+
+class Comment:
+    def __init__(self, author, text):
+        super().__init__()
+        self.author = author
+        self.text = text
+
+class VoteMixin:
+    def __init__(self):
+        self.votes = 0
+
+    def vote_up(self):
+        self.votes += 1
+
+
+class VoteablePost(Post, VoteMixin):
+    pass
+
+class VoteableComment(Comment, VoteMixin):
+    pass
+
+
+p = VoteablePost("me", "a title", "its text")
+c = VoteableComment("me", "a comment")
+
+for entry in [p, c]:
+    entry.vote_up()
+
+print(p.votes)
+print(c.votes)
+
+
+# ------------------------------
+# Participant mixin
+
+from abc import ABCMeta, abstractmethod
+
+class Server(metaclass=ABCMeta):
+    def listen(self):
+        print("listening on connection "+self.connection())
+
+    @abstractmethod
+    def connection(self):
+        pass
+
+
+class TCPConnectionMixin:
+    def connection(self):
+        return "TCP"
+
+
+class UDPConnectionMixin:
+    def connection(self):
+        return "UDP"
+
+
+class TCPServer(TCPConnectionMixin, Server):
+    pass
+
+class UDPServer(UDPConnectionMixin, Server):
+    pass
+
+
+tcp = TCPServer()
+udp = UDPServer()
+
+tcp.listen()
+udp.listen()
+
+
+
+
