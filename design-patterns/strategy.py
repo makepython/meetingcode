36107683f12f2ae7e161f29e202e47221d56a4e0
Stefano Borini <stefano.borini@gmail.com>
2019-10-21 20:47:43 +0100

    Added strategy

--- design-patterns/strategy.py
+++ design-patterns/strategy.py
@@ -1,1 +1,34 @@
 # add strategy bank example
+
+class NoDiscountStrategy:
+    def apply(self, price):
+        return price
+
+
+class DiscountStrategy:
+    def __init__(self, rate):
+        self._rate = rate
+    def apply(self, price):
+        return price * self._rate
+
+
+class Bill:
+    def __init__(self, customer):
+        self._customer = customer
+        self._total = 0.0
+        self.discount_strategy = NoDiscountStrategy()
+
+    def charge(self, amount):
+        self._total += self.discount_strategy.apply(amount)
+
+    def invoice(self):
+        print(f"{self._customer}: {self._total}")
+
+
+bill = Bill("Gates")
+bill.charge(100)
+bill.invoice()
+bill.discount_strategy = DiscountStrategy(0.8)
+bill.charge(100)
+bill.invoice()
+
