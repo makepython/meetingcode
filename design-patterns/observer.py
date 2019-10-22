94e04877738b612fb2d7edca085c4c88adf2a30d
Stefano Borini <stefano.borini@gmail.com>
2019-10-22 20:17:43 +0100

    Added observer

--- design-patterns/observer.py
+++ design-patterns/observer.py
@@ -1,22 +1,38 @@
 class Observable:
     def __init__(self):
         self._observers = []
+        self._value = 0
 
     def register_observer(self, observer):
         self._observers.append(observer)
 
-    def notify_observers(self, *args, **kwargs):
+    def _notify_observers(self):
         for observer in self._observers:
-            observer.notify(self, *args, **kwargs)
+            observer.notify(self)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value == self._value:
+            return
+
+        self._value = value
+        self._notify_observers()
+
 
 class Observer:
     def __init__(self, observable):
         observable.register_observer(self)
 
-    def notify(self, observable, *args, **kwargs):
-        print('Got', args, kwargs, 'From', observable)
+    def notify(self, observable):
+        print(str(observable) + ' changed: '+str(observable.value))
 
 
 subject = Observable()
 observer = Observer(subject)
-subject.notify_observers('test')
+subject.value = 5
+subject.value = 5
+subject.value = 8
